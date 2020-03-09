816142bfb610328fb5cc9b8d020814e8986c7934
Austin Heisey <ath9902@rit.edu>
2020-03-09 18:38:34 -0400

    started project

---
+++ Pneumonia_Machine_Learning/Pneumonia_Machine_Learning/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pneumonia_Machine_Learning
+//
+//  Created by Austin Heisey on 3/9/20.
+//  Copyright © 2020 Austin Heisey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
